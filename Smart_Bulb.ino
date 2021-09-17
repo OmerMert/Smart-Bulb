21a00d76df0ece1a1ea4619318be582fb67ed124
Ömer Mert Yurdakul <47865653+OmerMert@users.noreply.github.com>
2021-09-17 12:00:00 +0300

    ESP-01 code

--- Smart_Bulb.ino
+++ Smart_Bulb.ino
@@ -2,33 +2,22 @@
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <EEPROM.h>
 
 #define relay 0 //relay connected to  GPIO0
 
-char ssid[] = "SUPERONLINE_WiFi_8875";
-char pass[] = "HPVATVJU77PA";
-char auth[] = "W6790nsoiVeFDaMqAP5yO0ITdDYLkPOc";
+char ssid[] = ".........."; //wifi name
+char pass[] = ".........."; //wifi password
+char auth[] = ".........."; //Blynk token
 
-int EEaddress = 0; //EEPROM Address
-bool bulb_status = false;
 
 BLYNK_WRITE(V1)
 {
   int pinValue = param.asInt();
   
   if(pinValue)
-  {
-    bulb_status = true;
     digitalWrite(relay, HIGH);
-    EEPROM.write(EEaddress, bulb_status);
-  }
   else
-  {
-    bulb_status = false;
     digitalWrite(relay, LOW);
-    EEPROM.write(EEaddress, bulb_status);
-  }
   
 }
 
@@ -37,8 +26,7 @@ void setup()
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
 
-  pinMode(relay, OUTPUT);
-  bulb_status = EEPROM.read(EEaddress);
+  pinMode(relay, OUTPUT
 }
 
 void loop()
